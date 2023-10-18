8d3a23b81a848212322b2a36fa1a348d5053b632
Basharat Ali <basharatalidogar14@gmail.com>
2023-10-18 21:16:54 +0500

    data added

--- src/Layouts/Navbar.jsx
+++ src/Layouts/Navbar.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
+import React,{useState} from 'react'
 import "../Utilities/Navbar.css";
 import { Link } from 'react-router-dom'
 export default function Navbar() {
+  const [navbar, setNavbar]=useState(false);
+  const showButton=()=>{
+    console.log("Scrolled...");
+    if(window.scrollY>= 590){
+      setNavbar(false)
+    }
+    else{
+      setNavbar(true)
+    }
+  }
+  window.addEventListener('scroll',showButton);
   return (
     <>
-    <div id='header'>
+    <div id='header'className={navbar ? "active" :" navbars"} >
       <div className="container">
-      <nav class="navbar navbar-expand-lg navbar-light ">
+      <nav class="navbar navbar-expand-lg ">
   <img class="navbar-brand" src={process.env.PUBLIC_URL+"./main-logo.svg"} alt="My-Pic"  />
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
